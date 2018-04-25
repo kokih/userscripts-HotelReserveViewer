18bf7dd4217b3a936a2fc4856b142a4d5e143769
kokih <koki.h.dev@gmail.com>
2018-04-25 16:38:29 +0900

    Version 1.1.0 - ホテルごとで、背景色変更 - 喫煙・禁煙別で、背景色変更

--- src/HotelReserveViewer.user.js
+++ src/HotelReserveViewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HotelReserveViewer
 // @namespace    com.chuo.koki
-// @version      1.0.0
+// @version      1.1.0
 // @description  Improve view of Hotel alpha-one reservation page.
 // @author       Koki
 // @match        https://alpha1-u.hotels-system.jp/yoyakusearchg.aspx
@@ -17,6 +17,10 @@
     // Your code here...
     alert('Hello, monkey!');
 
+    // ========
+    // 機能1: テーブルをソートできるようにする
+    // ========
+
     // Table1 に予約情報が格納されている
     // 但し、tr 一つ目が <tr><th> とヘッダになっている。
     // tablesorter を機能させるには <thead> <tbody> が必要
@@ -25,11 +29,37 @@
     var trTag = $("table#Table1 tbody tr")[0];
     theadTag.append(trTag);
     $("#Table1").prepend(theadTag);
-
     var setStupidTable = function(){
         $('#Table1').tablesorter();
     };
-
     setTimeout(setStupidTable, 1500);
+
+    // ========
+    // 機能2: 予約先ホテル名ごとに、背景に色をつける
+    // ========
+    $("table#Table1 tbody tr").each(function(i, elem) {
+        //console.log(elem);
+        var targetTd = $(elem).find("td")[0];
+        var hotelname = targetTd.innerText;
+        if ( hotelname == "出雲" ) {
+            $(targetTd).css('background-color','honeydew');
+        } else if ( hotelname == "鯖江" ) {
+            $(targetTd).css('background-color','lightcyan');
+        }
+    });
+
+    // ========
+    // 機能3: 禁煙・喫煙に背景色をつける
+    // ========
+    $("table#Table1 tbody tr").each(function(i, elem) {
+        //console.log(elem);
+        var targetTd = $(elem).find("td")[1];
+        var smokableYN = targetTd.innerText;
+        if ( smokableYN.match(/喫煙/) ) {
+            $(targetTd).css('background-color','snow');
+        } else if ( smokableYN.match(/禁煙/) ) {
+            $(targetTd).css('background-color','azure');
+        }
+    });
 
 })(jQuery);
